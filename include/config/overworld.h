563572e4a8ae4d4544f22b5ba8bb57f75545d36d
natgeorose <nathanielpoland@gmail.com>
2025-11-25 15:16:54 -0700

    Initial text box changes
    Initial text box changes

--- include/config/overworld.h
+++ include/config/overworld.h
@@ -151,7 +151,7 @@
 
 // Text colors of Namebox. The numbers corresponds to the palette index.
 // The BG color is not provided as it always needs to be 0.
-#define OW_NAME_BOX_FOREGROUND_COLOR  1
-#define OW_NAME_BOX_SHADOW_COLOR      2
+#define OW_NAME_BOX_FOREGROUND_COLOR  15
+#define OW_NAME_BOX_SHADOW_COLOR      1
 
 #endif // GUARD_CONFIG_OVERWORLD_H
